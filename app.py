442dd0a91e7a9968485b2155d1ea18ef4b6056aa
evgeniitsvetkov <evgeniitsvetkov@yandex.ru>
2020-02-10 08:14:26 +0300

    Add app_context

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from models import db, User, Group, Applicant
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db.init_app(app)
 admin = Admin(app)
 
 admin.add_view(ModelView(User, db.session))
@@ -22,4 +21,7 @@ def index():
 
 
 if __name__ == '__main__':
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
     app.run()
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class Config:
     SECRET_KEY= "secret_key"
     SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
+    # postgres://xyybtbtfxjfvpw:942e4e024fdf4933ebc807ab5fa5f8889c05eddd02a128a2bbf488dcdc47a838@ec2-54-195-252-243.eu-west-1.compute.amazonaws.com:5432/d8cgf8nqkqm44l
     SQLALCHEMY_TRACK_MODIFICATIONS = False
