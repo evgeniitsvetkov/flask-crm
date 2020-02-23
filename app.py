10d943e768a6f1e787f87ef2754886352167123e
evgeniitsvetkov <evgeniitsvetkov@yandex.ru>
2020-02-23 19:02:20 +0300

    Add login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, redirect, render_template, request, session
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
@@ -23,6 +23,23 @@ def index():
 @app.route('/admin')
 def admin():
     return render_template('/admin/admin_dashboard.html')
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    error_msg = ""
+
+    if request.method == 'POST':
+        mail = request.form.get("mail")
+        password = request.form.get("password")
+
+        if (mail and password) and mail == 'user@mail.ru' and password == 'qwe123':
+            session["is_auth"] = True
+            return redirect('/')
+        else:
+            error_msg = "Неверное имя или пароль"
+
+    return render_template("auth.html", error_msg=error_msg)
 
 
 if __name__ == '__main__':
