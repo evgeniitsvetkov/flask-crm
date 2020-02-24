a3723eda0fe9a138879858866a34346931f87d34
evgeniitsvetkov <evgeniitsvetkov@yandex.ru>
2020-02-24 13:19:55 +0300

    Add logout and dashboard

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, render_template, request, session
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
 
 from config import Config
@@ -8,7 +8,15 @@ from models import db, User, MyUserView, Group, Applicant
 app = Flask(__name__)
 app.config.from_object(Config)
 
-admin = Admin(app)
+
+class DashboardView(AdminIndexView):
+
+    @expose('/')
+    def index(self):
+        return self.render('admin/admin_dashboard.html')
+
+
+admin = Admin(app, index_view=DashboardView())
 
 admin.add_view(MyUserView(User, db.session))
 admin.add_view(ModelView(Group, db.session))
@@ -17,12 +25,10 @@ admin.add_view(ModelView(Applicant, db.session))
 
 @app.route('/')
 def index():
-    return 'Главная страница'
+    if not session.get('is_auth'):
+        return redirect('/login')
 
-
-@app.route('/admin')
-def admin():
-    return render_template('/admin/admin_dashboard.html')
+    return '<a href="/admin">Панель администратора</a>'
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -40,6 +46,23 @@ def login():
             error_msg = "Неверное имя или пароль"
 
     return render_template("auth.html", error_msg=error_msg)
+
+
+@app.route('/logout', methods=["POST"])
+def logout():
+    if session.get("is_auth"):
+        session.pop("is_auth")
+    return redirect("/login")
+
+
+@app.errorhandler(404)
+def render_not_found():
+    return "Ничего не нашлось! Вот неудача, отправляйтесь на главную!"
+
+
+@app.errorhandler(500)
+def render_server_error(error):
+    return "Что-то не так, но мы все починим:\n{}".format(error.original_exception), 500
 
 
 if __name__ == '__main__':
